bfd3d8cdb20686ce8b2fb1859d2bd7ebb5092cd8
seelin <seelin@126.com>
2024-12-16 20:42:24 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -2,9 +2,10 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-SK_CORPID = os.environ.get('SK_CORPID')
+
 @app.route('/')
 def hello_world():
+   SK_CORPID = os.environ.get('SK_CORPID')
    return 'Hello World'+SK_CORPID
 
 if __name__ == '__main__':
