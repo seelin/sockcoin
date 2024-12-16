c4f1e9eab42afab5c0c9d1434dc4e5b46d05a755
seelin <seelin@126.com>
2024-12-16 22:25:52 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -4,12 +4,12 @@ import time
 import requests
 import json
 app = Flask(__name__)
+SK_ESTOKEN=''
 
 @app.route('/')
-
 def hello_world():
    SK_CORPID = os.environ.get('SK_CORPID')
-   return 'Hello World'+SK_CORPID
+   return 'Hello World'
 
 @app.route('/getbal',methods=['GET'])
 def getbal():
@@ -19,6 +19,13 @@ def getbal():
    res=requests.get(url)
    rjson=res.json()
    return 'bal'+rjson['result']
+
+@app.route('/getprice',methods=['GET'])
+def getprice():
+   url="https://api.etherscan.io/api?module=stats&action=ethprice&apikey="+SK_ESTOKEN
+   res=requests.get(url)
+   return res.text
    
 if __name__ == '__main__':
+   SK_ESTOKEN= os.environ.get('SK_ESTOKEN')
    app.run('0.0.0.0',82,True)
