481621fe5ef788d6ec1ccd8133e4522ce9e0242a
seelin <seelin@126.com>
2024-12-16 21:53:03 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -6,9 +6,12 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello_world():
    SK_CORPID = os.environ.get('SK_CORPID')
    return 'Hello World'+SK_CORPID
+
+@app.route('/getbal')
 def getbal():
    SK_ESTOKEN = os.environ.get('SK_ESTOKEN')
    url="https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey="+SK_ESTOKEN
