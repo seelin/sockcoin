3ebd58264cbd6372ce9a50f8878ee3036b3738df
seelin <seelin@126.com>
2024-12-21 18:09:45 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -60,9 +60,11 @@ def getcmcurl():
    else:
       partxt=str(base64.b64decode(partxt), 'utf-8')
       parameters=json.loads(partxt)
+      
    print(tourl)
    print(partxt)
    print(parameters)
+   
    url = 'https://pro-api.coinmarketcap.com'+tourl
    session = requests.Session()
    session.headers.update(headers)
